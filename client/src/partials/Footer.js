cf919aac0eb40fdfd303034283be5891a05697b4
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-04-27 10:07:12 +0530

    initial commit

---
+++ client/src/partials/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-content">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/blogs">Blogs</a>
+        </li>
+        <li>
+          <a href="/login">Account</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+);
+
+export default Footer;
